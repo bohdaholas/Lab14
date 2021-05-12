aaeef587ddc2ae951e25fddab07e8553a41c6e7f
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-05-12 21:31:25 +0300

    task 1 and 2 of lab14

---
+++ linkedbst.py
@@ -0,0 +1,294 @@
+"""
+File: linkedbst.py
+Author: Ken Lambert
+"""
+
+from abstractcollection import AbstractCollection
+from bstnode import BSTNode
+from linkedstack import LinkedStack
+from math import log2
+
+
+class LinkedBST(AbstractCollection):
+    """An link-based binary search tree implementation."""
+
+    def __init__(self, source_collection=None):
+        """Sets the initial state of self, which includes the
+        contents of sourceCollection, if it's present."""
+        self._root = None
+        AbstractCollection.__init__(self, source_collection)
+
+    # Accessor methods
+    def __str__(self):
+        """Returns a string representation with the tree rotated
+        90 degrees counterclockwise."""
+
+        def recurse(node, level):
+            s = ""
+            if node is not None:
+                s += recurse(node.right, level + 1)
+                s += "| " * level
+                s += str(node.data) + "\n"
+                s += recurse(node.left, level + 1)
+            return s
+
+        return recurse(self._root, 0)
+
+    def __iter__(self):
+        """Supports a preorder traversal on a view of self."""
+        if not self.isEmpty():
+            stack = LinkedStack()
+            stack.push(self._root)
+            while not stack.isEmpty():
+                node = stack.pop()
+                yield node.data
+                if node.right is not None:
+                    stack.push(node.right)
+                if node.left is not None:
+                    stack.push(node.left)
+
+    def inorder(self):
+        """Supports an inorder traversal on a view of self."""
+        lyst = []
+
+        def recurse(node):
+            if node is not None:
+                recurse(node.left)
+                lyst.append(node.data)
+                recurse(node.right)
+
+        recurse(self._root)
+        return iter(lyst)
+
+    def __contains__(self, item):
+        """Returns True if target is found or False otherwise."""
+        return self.find(item) is not None
+
+    def find(self, item):
+        """If item matches an item in self, returns the
+        matched item, or None otherwise."""
+
+        def recurse(node):
+            if node is None:
+                return None
+            elif item == node.data:
+                return node.data
+            elif item < node.data:
+                return recurse(node.left)
+            else:
+                return recurse(node.right)
+
+        return recurse(self._root)
+
+    # Mutator methods
+    def clear(self):
+        """Makes self become empty."""
+        self._root = None
+        self._size = 0
+
+    def add(self, item):
+        """Adds item to the tree."""
+
+        # Helper function to search for item's position
+        def recurse(node):
+            # New item is less, go left until spot is found
+            if item < node.data:
+                if node.left is None:
+                    node.left = BSTNode(item)
+                else:
+                    recurse(node.left)
+            # New item is greater or equal,
+            # go right until spot is found
+            elif node.right is None:
+                node.right = BSTNode(item)
+            else:
+                recurse(node.right)
+                # End of recurse
+
+        # Tree is empty, so new item goes at the root
+        if self.isEmpty():
+            self._root = BSTNode(item)
+        # Otherwise, search for the item's spot
+        else:
+            recurse(self._root)
+        self._size += 1
+
+    def remove(self, item):
+        """Precondition: item is in self.
+        Raises: KeyError if item is not in self.
+        postcondition: item is removed from self."""
+        if item not in self:
+            raise KeyError("Item not in tree.""")
+
+        # Helper function to adjust placement of an item
+        # noinspection PyPep8Naming
+        def liftMaxInLeftSubtreeToTop(top):
+            # Replace top's datum with the maximum datum in the left subtree
+            # Pre:  top has a left child
+            # Post: the maximum node in top's left subtree
+            #       has been removed
+            # Post: top.data = maximum value in top's left subtree
+            parent = top
+            current_node = top.left
+            while current_node.right is not None:
+                parent = current_node
+                current_node = current_node.right
+            top.data = current_node.data
+            if parent == top:
+                top.left = current_node.left
+            else:
+                parent.right = current_node.left
+
+        # Begin main part of the method
+        if self.isEmpty():
+            return None
+
+        # Attempt to locate the node containing the item
+        item_removed = None
+        pre_root = BSTNode(None)
+        pre_root.left = self._root
+        parent = pre_root
+        direction = 'L'
+        current_node = self._root
+        while current_node is not None:
+            if current_node.data == item:
+                item_removed = current_node.data
+                break
+            parent = current_node
+            if current_node.data > item:
+                direction = 'L'
+                current_node = current_node.left
+            else:
+                direction = 'R'
+                current_node = current_node.right
+
+        # Return None if the item is absent
+        if item_removed is None:
+            return None
+
+        # The item is present, so remove its node
+
+        # Case 1: The node has a left and a right child
+        #         Replace the node's value with the maximum value in the
+        #         left subtree
+        #         Delete the maximium node in the left subtree
+        if current_node.left is not None \
+                and current_node.right is not None:
+            liftMaxInLeftSubtreeToTop(current_node)
+        else:
+
+            # Case 2: The node has no left child
+            if current_node.left is None:
+                new_child = current_node.right
+
+                # Case 3: The node has no right child
+            else:
+                new_child = current_node.left
+
+                # Case 2 & 3: Tie the parent to the new child
+            if direction == 'L':
+                parent.left = new_child
+            else:
+                parent.right = new_child
+
+        # All cases: Reset the root (if it hasn't changed no harm done)
+        #            Decrement the collection's size counter
+        #            Return the item
+        self._size -= 1
+        if self.isEmpty():
+            self._root = None
+        else:
+            self._root = pre_root.left
+        return item_removed
+
+    def replace(self, item, new_item):
+        """
+        If item is in self, replaces it with newItem and
+        returns the old item, or returns None otherwise."""
+        probe = self._root
+        while probe is not None:
+            if probe.data == item:
+                old_data = probe.data
+                probe.data = new_item
+                return old_data
+            elif probe.data > item:
+                probe = probe.left
+            else:
+                probe = probe.right
+        return None
+
+    def height(self):
+        """
+        Return the height of tree
+        :return: int
+        """
+        root = self._root
+
+        def height1(node):
+            """
+            Helper function
+            :param node:
+            :return:
+            """
+            if node is None:
+                return -1
+            else:
+                left_sub_tree_depth = height1(node.left)
+                right_sub_tree_depth = height1(node.right)
+            if right_sub_tree_depth > left_sub_tree_depth:
+                return right_sub_tree_depth + 1
+            else:
+                return left_sub_tree_depth + 1
+        return height1(root)
+
+    def is_balanced(self):
+        """
+        Return True if tree is balanced
+        :return:
+        """
+        return self.height() < 2 * log2(len(list(self.inorder())) + 1) - 1
+
+    def range_find(self, low, high):
+        """
+        Returns a list of the items in the tree, where low <= item <= high."""
+        return [node for node in self.inorder() if low <= node <= high]
+
+    def rebalance(self):
+        """
+        Rebalances the tree.
+        :return:
+        """
+        lst = list(self.inorder())
+        self.clear()
+        while lst:
+            node_pos = len(lst) // 2
+            self.add(lst[node_pos])
+            lst.remove(lst[node_pos])
+
+    def successor(self, item):
+        """
+        Returns the smallest item that is larger than
+        item, or None if there is no such item.
+        :param item:
+        :type item:
+        :return:
+        :rtype:
+        """
+        try:
+            return min([node for node in self if node > item])
+        except ValueError:
+            return None
+
+    def predecessor(self, item):
+        """
+        Returns the largest item that is smaller than
+        item, or None if there is no such item.
+        :param item:
+        :type item:
+        :return:
+        :rtype:
+        """
+        try:
+            return max([node for node in self if node < item])
+        except ValueError:
+            return None
